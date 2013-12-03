f67b6cfdca057d70f26f9b1e5e6dcae36cc7a0cd
phenix502 <huangcong502@gmail.com>
2013-12-03 11:30:19 +0800

    使用随机森林提取最重要的100个特征

--- classifyMusic.r
+++ classifyMusic.r
@@ -1,4 +1,6 @@
 #加载包
+# Fselector，用随机森林算法选择最重要的前几个变量
+# 此文档为重要分析文档，会调用别的函数
 library(tm)
 library(Rwordseg)
 library(RTextTools)
@@ -16,63 +18,44 @@ song.sweet <- Infor.sweet$lyric
 song.sad <- Infor.sad$lyric
 
 
-# 形成DocumentTermMatrix
+# 形成DocumentTermMatrix， 调入dtm.r 文件
 corpus.dtm <- dtm(corpus)
 corpus.dtm.tfidf <- dtm(corpus, tfidf=TRUE)
-# 情感词典
+
+# 加载情感词典
 corpus.emotion.dtm <- dtm(corpus.emotion)
 
 
 # 转为data frame
 corpus.df <- as.data.frame(inspect(corpus.dtm))
+# 标注标签
 label <- c(rep("sad",764), rep("sweet",861))
 label <- factor(label)
-# 标注标签
 corpus.df$label <- label
+
+
 # 卡方检验
 weights.chi <- chi.squared(label~., corpus.df)
 subset.chi <- cutoff.k(weights.chi, 200)
 f.chi <- as.simple.formula(subset.chi, "label")
 
 # information gain
-
 weights.in <- information.gain(label~., corpus.df)
 subset.in <- cutoff.k(weights.in, 200)
 f.in <- as.simple.formula(subset.in, "label")
 
-weights.rf <- random.forest.importance(label~., corpus.df)
-#rownames(song.corpus.dataframe) <- 1:nrow(song.corpus.dataframe)
-#song.corpus.dataframe.tfidf <- as.data.frame(inspect(song.corpus.dtm.tfidf))
-#rownames(song.corpus.dataframe.tfidf) <- 1:nrow(song.corpus.dataframe.tfidf)
+
+## 随机森林算法选取前100个重要的词语
+weights.rf <- random.forest.importance(label~., corpus.df, 1)
+subset <- cutoff.k(weights.rf, 100)
+f <- as.simple.formula(subset, "class")
 
 
 # 提取出来的特征做为一个新的数据框
-
-feature.chi <- names(corpus.df) %in% subset.chi
-df.chi <- corpus.df[feature.chi] 
-
-model3 <- ksvm(label~.,data=df.chi,kernel='rbfdot')
-plot(model3,data=df.chi)
-
-## 数据分析阶段
-
-
-
-
-
-
-
-
-
-
-# 形成DocTermMatrix
-song.sweet.dtm <- dtm(song.sweet)$DocTermMatrix
-song.sad.dtm <- dtm(song.sad)$DocTermMatrix
-
-# 画词云
-
-song.sweet.wordcloud <- wordCloud(song.sweet.dtm)
-song.sad.wordcloud <- wordCloud(song.sad.dtm)
-
+d <- data.frame(word = rownames(weights.rf), freq= weights.rf$attr_importance)
+# 按重要性从大到小排列
+newdata <- d[order(-d$freq),]
+#画出词云
+wordcloud(d$word, d$freq, random.order = F, colors = brewer.pal(8, "Dark2"))
 
 
