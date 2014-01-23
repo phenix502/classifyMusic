f9191140998a234b9e699428cae80ce137c65c98
phenix502 <huangcong502@gmail.com>
2014-01-23 18:59:55 +0800

    可以提取特征，提取的特征能显著改善分类效果

--- analysis.r
+++ analysis.r
@@ -1,50 +1,26 @@
 
 # 1 表示sad 2表示sweet
 label <- c(rep(1L,764), rep(2L, 861))
-
+lable <- factor(label)
+#  从伤感歌词中挑选64首作为测试集，同理甜蜜类挑选61首作为测试集
 sad.test <- sample(1:764, 64, replace= FALSE)
 sweet.test <-sample(765:1625,61,replace= FALSE)
-test <- c(sad.test,sweet.test)
+testSize <- c(sad.test,sweet.test)
+trainSize <- 1:1625
+trainSize <- trainSize[-test]
 # create a container
-container.song <- create_container(corpus.dtm.tfidf, label,trainSize=1:1625[-test], testSize=test, virgin=FALSE)
+container.song <- create_container(corpus.dtm.tfidf, label,
+                                   trainSize=trainSize, testSize=testSize, virgin=FALSE)
 
 # training models
-TREE.song <- train_model(container.song, "TREE")
 SVM.song <- train_model(container.song, "SVM")
-MAXENT.song <- train_model(container.song, "MAXENT")
-
-GLMNET.song <- train_model(container.song, "GLMNET")
-SLDA <- train_model(container.song, "SLDA")
-BOOSTING.song <- train_model(container.song, "BOOSTING")
-BAGGING.song <- train_model(container.song, "BAGGING")
 RF.song <- train_model(container.song, "RF")
-NNET.song <- train_model(container.song, "NNET")
-
 
 # classifying data using trained models
 SVM_CLASSIFY.song <- classify_model(container.song, SVM.song)
-TREE_CLASSIFY.song <- classify_model(container.song, TREE.song)
-MAXENT_CLASSIFY.song <- classify_model(container.song, MAXENT.song)
-
-GLMNET_CLASSIFY.song <- classify_model(container.song, GLMNET.song)
-# SLDA_CLASSIFY <- classify_model(container.song, SLDA)
-BOOSTING_CLASSIFY <- classify_model(container.song, BOOSTING.song)
-BAGGING_CLASSIFY <- classify_model(container.song, BAGGING.song)
-RF_CLASSIFY <- classify_model(container.song, RF.song)
-NNET_CLASSIFY <- classify_model(container.song, NNET.song)
+RF_CLASSIFY.song <- classify_model(container.song, RF.song)
 
 
-CLASSIFY.song <- cbind(SVM_CLASSIFY.song,MAXENT_CLASSIFY.song,TREE_CLASSIFY.song, RF_CLASSIFY
-                       ,GLMNET_CLASSIFY.song, BOOSTING_CLASSIFY, BAGGING_CLASSIFY)
+create_precisionRecallSummary(container=container.song, classification_results=SVM_CLASSIFY.song)
+create_precisionRecallSummary(container=container.song, classification_results=RF_CLASSIFY.song)
 
-# analytics
-analytics.song <- create_analytics(container.song,CLASSIFY.song)
-
-
-newdata <- subset(analytics.song@document_summary, select=c(SVM_LABEL, FORESTS_LABEL, MANUAL_CODE, CONSENSUS_CODE))
-a <- newdata$SVM_LABEL==newdata$MANUAL_CODE
-precision <- length(a[a==TRUE])/length(newdata$SVM_LABEL)
-
-
-table(newdata$MANUAL_CODE, newdata$CONSENSUS_CODE)
-table(newdata$MANUAL_CODE, newdata$SVM_LABEL)
--- classifyMusic.r
+++ classifyMusic.r
@@ -10,45 +10,27 @@ library(FSelector)
 # 防止读入的所有string都被当成factor
 options(stringsAsFactors=FALSE)
 # 读入csv文件
-Infor.sweet <- read.csv('sweetsong.csv', header=TRUE)
-Infor.sad <- read.csv('sadsong.csv', header=TRUE)
+Infor.sweet <- read.csv('material/sweetsong.csv', header=TRUE)
+Infor.sad <- read.csv('material/sadsong.csv', header=TRUE)
 
-# 取歌词lyric进行分析
-song.sweet <- Infor.sweet$lyric
-song.sad <- Infor.sad$lyric
+# 分词并形成语料库
+corpus <- makeCorpus(Infor.sweet$lyric, Infor.sad$lyric)
 
-
-# 形成DocumentTermMatrix， 调入dtm.r 文件
-corpus.dtm <- dtm(corpus)
+# 形成DocumentTermMatrix
 corpus.dtm.tfidf <- dtm(corpus, tfidf=TRUE)
-
-# 加载情感词典
-corpus.emotion.dtm <- dtm(corpus.emotion)
 
 
 # 转为data frame
-corpus.df <- as.data.frame(inspect(corpus.dtm))
-# 标注标签
-label <- c(rep("sad",764), rep("sweet",861))
-label <- factor(label)
-corpus.df$label <- label
-
-
-# 卡方检验
-weights.chi <- chi.squared(label~., corpus.df)
-subset.chi <- cutoff.k(weights.chi, 200)
-f.chi <- as.simple.formula(subset.chi, "label")
-
-# information gain
-weights.in <- information.gain(label~., corpus.df)
-subset.in <- cutoff.k(weights.in, 200)
-f.in <- as.simple.formula(subset.in, "label")
-
+corpus.df <- as.data.frame(inspect(corpus.dtm.tfidf))
 
 ## 随机森林算法选取前100个重要的词语
 weights.rf <- random.forest.importance(label~., corpus.df, 1)
 subset <- cutoff.k(weights.rf, 100)
-f <- as.simple.formula(subset, "class")
+
+
+## 把提取的特征作为新的docment-term matrix
+feature.df <- as.DocumentTermMatrix(corpus.df[subset],weighting=weightTf)
+
 
 
 # 提取出来的特征做为一个新的数据框
